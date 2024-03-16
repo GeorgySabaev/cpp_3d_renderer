f48a666e96146358a595063b36cfb7782ef061a2
SaGeDeveloper <gasabaev@edu.hse.ru>
2024-03-17 00:44:57 +0300

    patch: update triangle method to use type alias

--- src/Triangle.cpp
+++ src/Triangle.cpp
@@ -1,11 +1,12 @@
 #include "Triangle.hpp"
 
-Eigen::Vector3f cpp_renderer::Triangle::GetSurfacePoint(const Eigen::Vector2f &uv) const
-{
-    auto pivot = points[0];
-    Eigen::Matrix<float, 3, 2> uv_space;
-    uv_space.col(0) = points[1] - pivot;
-    uv_space.col(1) = points[2] - pivot;
+namespace cpp_renderer {
+Triangle::Vector3 Triangle::GetSurfacePoint(const Vector2 &uv) const {
+  auto pivot = points[0];
+  Eigen::Matrix<float, 3, 2> uv_space;
+  uv_space.col(0) = points[1] - pivot;
+  uv_space.col(1) = points[2] - pivot;
 
-    return (uv_space * uv) + pivot;
+  return (uv_space * uv) + pivot;
 }
+} // namespace cpp_renderer
